6e330772c5278b3de157af1a1692c26033205f42
lahfamostafa <lahfa.mostafa1@gmail.com>
2026-03-17 11:06:27 +0000

    feat: définition des routes API

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register',[UserController::class,'register']);
+Route::post('/login',[UserController::class,'login']);
+
+Route::middleware('auth:sanctum')->group(function (){
+});
