174af747745fae5d87124b2dd7f5d512fdd4d559
lahfamostafa <lahfa.mostafa1@gmail.com>
2026-03-17 11:06:19 +0000

    feat: ajout du trait pour uniformiser les réponses API

---
+++ app/Traits/ResponseTrait.php
@@ -0,0 +1,22 @@
+<?php 
+
+    namespace App\Traits;
+    trait ResponseTrait{
+        public function successResponse($data =[],$message){
+            return response()->json([
+                'success' => true,
+                'data' => $data,
+                'message' => $message
+            ]);
+        }
+        
+        public function errorResponse($data =[],$message){
+            return response()->json([
+                'success' => false,
+                'data' => $data,
+                'message' => $message
+            ]);
+        }
+    }
+
+?>
