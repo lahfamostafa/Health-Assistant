9b72bdf89175c95f8ba0f3e129e26ce551daf109
lahfamostafa <lahfa.mostafa1@gmail.com>
2026-03-24 15:23:03 +0100

    Ajout du système d'authentification (login, register, logout) avec Sanctum

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use App\Traits\ResponseTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
@@ -21,11 +23,29 @@ class UserController extends Controller
             ]);
             return $this->successResponse($user,'register reusit !');
         } catch (\Exception $e) {
-            return $this->errorResponse($e->getMessage(),'données invalid');
+            return $this->errorResponse($e->getMessage());
         }
     }
 
-    public function login(Request $request){
-        
+    public function login(LoginRequest $request){
+        $info = $request->only('email','password');
+        if(Auth::attempt($info)){
+            $user = Auth::user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return $this->successResponse(['user'=> $user, 'token'=> $token],'login avec success');
+        }else
+            return $this->errorResponse('données invalide');
+    }
+
+    public function logout(Request $request){
+        $request->user()->currentAccessToken()->delete();
+        return $this->successResponse([],"vous ete déconnecté");
+    }
+
+    public function user(){
+        $user = User::where('id',Auth::id())->get();
+        if($user) return $this->successResponse($user,'mon profile');
+        else return $this->errorResponse('vous ete non connecté');
     }
 }
